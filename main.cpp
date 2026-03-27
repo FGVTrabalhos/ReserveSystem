884baeb03d996d31365e14184a66bc84f630e28a
Luiz Lima <luizantonio.9ano@gmail.com>
2026-03-27 19:15:05 -0300

    correção testes

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include "ReservationSystem.hpp"
 
 
-#define SALAS 5
+#define SALAS 3
 
 int main (void){
     // Criação do Sistema
-    int capacities[SALAS] = {30, 100}; 
+    int capacities[SALAS] = {30, 50, 100}; 
     ReservationSystem sistema(SALAS, capacities);
+
 
     // Reservas bem sucedidas
     ReservationRequest calculus("Vetorial Calculus", "segunda", 9, 11, 20);
@@ -24,9 +25,21 @@ int main (void){
     ReservationRequest diferencialequations("Ordinary Diferencial Equations", "terca", 9, 11, 20);
     sistema.reserve(diferencialequations);
 
-    // Tentativa de reserva sem disponibilidade
-    ReservationRequest englishclass("English Class - Book 7", "segunda", 7, 9, 30);
-    sistema.reserve(englishclass);
+
+    // Tentativa de reserva sem disponibilidade (Lotando todas as salas segundas às 7:00)
+    ReservationRequest englishclass2("English Class - Book 2", "segunda", 7, 9, 30);
+    sistema.reserve(englishclass2);
+
+    ReservationRequest englishclass7("English Class - Book 7", "segunda", 7, 9, 30);
+    sistema.reserve(englishclass7);
+
+    ReservationRequest englishclass9("English Class - Book 9", "segunda", 7, 9, 30);
+    sistema.reserve(englishclass9);
+
+    // Não funciona (3 salas ocupadas)
+    ReservationRequest englishclass10("English Class - Book 10", "segunda", 7, 9, 30);
+    sistema.reserve(englishclass10);
+
 
     // Tentativa de reserva extrapolando o limite da sala
     ReservationRequest lecture("Lecture with Cesár Camacho", "sexta", 14, 16, 250);
