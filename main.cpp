349d6eccd3a6360c33421ebf4e76dc1ff9c31439
Luiz Lima <luizantonio.9ano@gmail.com>
2026-03-28 11:31:36 -0300

    Modularizando os testes

--- main.cpp
+++ main.cpp
@@ -3,12 +3,10 @@
 
 #define SALAS 3
 
-int main (void){
-    // Criação do Sistema
-    int capacities[SALAS] = {30, 50, 100}; 
-    ReservationSystem sistema(SALAS, capacities);
+// ================================================================================
+// Funções para os testes
 
-
+void testSuccessfulReservations(ReservationSystem& sistema) {
     // Reservas bem sucedidas
     ReservationRequest calculus("Vetorial Calculus", "segunda", 9, 11, 20);
     sistema.reserve(calculus);
@@ -25,7 +23,20 @@ int main (void){
     ReservationRequest diferencialequations("Ordinary Diferencial Equations", "terca", 9, 11, 20);
     sistema.reserve(diferencialequations);
 
-
+    sistema.printSchedule();
+    sistema.cancel("Vetorial Calculus");
+    sistema.cancel("Data Structures");
+    sistema.cancel("Numerical Linear Algebra");
+    sistema.cancel("Theory of probability");
+    sistema.cancel("Ordinary Diferencial Equations");
+};
+void testRoomCapacityLimit(ReservationSystem& sistema) {
+    // Tentativa de reserva extrapolando o limite da sala
+    ReservationRequest lecture("Lecture with Cesár Camacho", "sexta", 14, 16, 250);
+    sistema.reserve(lecture);
+    sistema.printSchedule();
+};
+void testOverbooking(ReservationSystem& sistema) {
     // Tentativa de reserva sem disponibilidade (Lotando todas as salas segundas às 7:00)
     ReservationRequest englishclass2("English Class - Book 2", "segunda", 7, 9, 30);
     sistema.reserve(englishclass2);
@@ -40,15 +51,32 @@ int main (void){
     ReservationRequest englishclass10("English Class - Book 10", "segunda", 7, 9, 30);
     sistema.reserve(englishclass10);
 
-
-    // Tentativa de reserva extrapolando o limite da sala
-    ReservationRequest lecture("Lecture with Cesár Camacho", "sexta", 14, 16, 250);
-    sistema.reserve(lecture);
-
+    sistema.printSchedule();
+    sistema.cancel("English Class - Book 2");
+    sistema.cancel("English Class - Book 7");
+    sistema.cancel("English Class - Book 9");
+};
+void testCancellation(ReservationSystem& sistema) {
     // Cancelamento de reserva
-    sistema.cancel("Vetorial Calculus");
+    ReservationRequest calculus("Vetorial Calculus", "segunda", 9, 11, 20);
 
-    // Visualizando grade
-    sistema.printSchedule(); 
+    sistema.reserve(calculus);
+    sistema.printSchedule();
+    sistema.cancel("Vetorial Calculus");
+    sistema.printSchedule();
+};
+
+
+int main (void){
+    // Criação do Sistema
+    int capacities[SALAS] = {30, 50, 100}; 
+    ReservationSystem sistema(SALAS, capacities);
+
+    // Realizando os Testes
+    testSuccessfulReservations(sistema);
+    testRoomCapacityLimit(sistema);
+    testOverbooking(sistema);
+    testCancellation(sistema);
+    
     return 0;
 }
